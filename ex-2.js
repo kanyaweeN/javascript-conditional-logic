cd4f0a2dc1badb9cdb06f8ebf6c3cef013c287c3
Preecha <preecha.sayang@gmail.com>
2025-07-18 14:00:05 +0700

    fix: correct lightBulbStatus casing and enhance conditional logic in exercises

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,15 @@
 //Exercise 2
-let lightBulbStatus = "On";
+let lightBulbStatus = "on";
 
 // Start coding here.
+if(lightBulbStatus === "on"){
+    console.log("Light bulb is On.")
+}else if (lightBulbStatus === "off"){
+    console.log("Light bulb is Off.")
+}else if (lightBulbStatus === "Broken"){
+    console.log("Light bulb is Broken.")
+}else {
+    console.log(`Please choose the correct input (On/Off/Broken)`)
+}
+
+lightBulbStatus = "Broken"
