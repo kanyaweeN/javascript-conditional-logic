bf92aeef978ff8a6c9ba84ead1d08dba26fe54af
lalinyuuu <lullalin.yu@gmail.com>
2025-07-18 14:09:55 +0700

    feat: ex1-7

--- ex-2.js
+++ ex-2.js
@@ -2,3 +2,21 @@
 let lightBulbStatus = "On";
 
 // Start coding here.
+// ถ้าสถานะของหลอดไฟ (lightBulbStatus) เป็น “Broken"ให้โปรแกรมหลอดไฟแสดงข้อความว่า 
+// "Light bulb is Broken." ออกมาทางหน้าจอด้วย console.log()
+// ให้ Reassign ตัว Value ของ lightBulbStatus เพื่อให้แสดงผลลัพธ์ทาง 
+// console.log() ออกมาเป็น "Light bulb is Broken."
+//กรณีที่ไม่เข้าเงื่อนไขใด ๆ เลยให้โปรแกรมหลอดไฟแสดงข้อความว่า “Please choose the correct input (On/Off/Broken)” 
+// ออกมาทางหน้าจอด้วย console.log()
+
+lightBulbStatus = "Broken";
+if (lightBulbStatus === "On") {
+    console.log("Light bulb is On.");
+  } else if (lightBulbStatus === "Off") {
+    console.log("Light bulb is Off.");
+  } else if (lightBulbStatus === "Broken") {
+    console.log("Light bulb is Broken.");
+  } else {
+    console.log("Please choose the correct input (On/Off/Broken)");
+  }
+  
